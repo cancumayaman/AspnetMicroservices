b5a565632ae5d01e7845769e82a0fafbd4427d89
CanCumaYaman <65663050+CanCumaYaman@users.noreply.github.com>
2021-06-02 02:05:09 +0300

    Dependency Injections created.

---
+++ src/Services/Ordering/Ordering.Application/ApplicationServiceRegistration.cs
@@ -0,0 +1,26 @@
+﻿using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using Ordering.Application.Behaviours;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ordering.Application
+{
+   public static class ApplicationServiceRegistration
+    {
+        public static IServiceCollection AddApplicationService(this IServiceCollection services)
+        {
+            services.AddAutoMapper(Assembly.GetExecutingAssembly());
+            services.AddValidatorsFromAssembly(Assembly.GetExecutingAssembly());
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(UnHandledExceptionBehaviour<,>));
+            services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationBehaviour <,>));
+            return services;
+        }
+    }
+}
