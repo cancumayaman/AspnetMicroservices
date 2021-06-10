3a1f51bdf7e5fc1097573364934ab1d6a9f6d1df
CanCumaYaman <65663050+CanCumaYaman@users.noreply.github.com>
2021-06-10 17:34:56 +0300

    Consumer settings implemented in Ordering.API startup.

---
+++ src/Services/Ordering/Ordering.API/EventBusConsumer/BasketCheckoutConsumer.cs
@@ -0,0 +1,17 @@
+﻿using EventBus.Messages.Events;
+using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ordering.API.EventBusConsumer
+{
+    public class BasketCheckoutConsumer : IConsumer<BasketCheckoutEvent>
+    {
+        public Task Consume(ConsumeContext<BasketCheckoutEvent> context)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
