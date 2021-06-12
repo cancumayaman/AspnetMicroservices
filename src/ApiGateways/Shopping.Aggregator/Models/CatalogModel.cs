a9d47978c54fde498c866d7172d4c8f6f9e1cec6
CanCumaYaman <65663050+CanCumaYaman@users.noreply.github.com>
2021-06-13 00:34:50 +0300

    Models created in shopping.aggregator

---
+++ src/ApiGateways/Shopping.Aggregator/Models/CatalogModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Shopping.Aggregator.Models
+{
+    public class CatalogModel
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Category { get; set; }
+        public string Summary { get; set; }
+        public string Description { get; set; }
+        public string ImageFile { get; set; }
+        public decimal Price { get; set; }
+    }
+}
